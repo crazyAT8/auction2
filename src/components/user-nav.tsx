4045fd892089e47e42a6f87cc6ded70cdcb3aa7b
crazyAT8 <88wwatson@gmail.com>
2025-02-15 23:47:41 -0600

    btn and dropdown menu edits 2/15/25

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -16,7 +16,7 @@ export function UserNav() {
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/placeholder.svg" alt="@user" />
-            <AvatarFallback>U</AvatarFallback>
+            <AvatarFallback>User-Nav</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
